02f8ae5346e82238c240ac80702b0bec9f9b4d66
xamgore <xamgore@ya.ru>
2017-03-05 15:51:14 +0300

    Remove redundant code (swipe)

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,6 @@
 <script>
 import Navbar from './components/Navbar'
 
-let lastX, lastY, lastTime
-
 export default {
   name: 'app',
   components: { Navbar },
@@ -31,40 +29,6 @@ export default {
       this.direction = goBack ? 'slide-right' : 'slide-left'
       next()
     })
-  },
-  methods: {
-    start(e) {
-      const touch = e.changedTouches[0]
-      lastX = touch.pageX
-      lastY = touch.pageY
-      lastTime = new Date().getTime()
-    },
-    move(e) {
-      const touch = e.changedTouches[0]
-      const dx = touch.pageX - lastX
-      const dy = touch.pageY - lastY
-
-      const now = new Date().getTime()
-      const time = now - lastTime
-      const velocityX = Math.abs(dx) / time
-
-      const correctAngle = Math.abs(dx) / Math.abs(dy) > 2
-      const enoughPower = velocityX >= 0.6
-      const enoughDistance = dx >= 15
-
-      lastX = touch.pageX
-      lastY = touch.pageY
-      lastTime = now
-
-      const swiperight = correctAngle && enoughPower && enoughDistance
-      if (!swiperight) return
-
-      if (this.$route.name === 'settings' && window.history.length > 2) {
-        return this.$router.go(-1)
-      }
-
-      this.$router.push('/')
-    }
   }
 }
 </script>
